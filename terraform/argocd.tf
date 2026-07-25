22ceec693ba21b716ab0bf310b93299ededc2aa1
Aoun62336 <aounmd74@gmail.com>
2026-07-25 21:59:29 +0530

    feat: add Helm chart for AssembleMonitor backend, frontend, Grafana, and OTEL collector with ArgoCD configuration

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -24,22 +24,67 @@ resource "helm_release" "argocd" {
     value = "ClusterIP"
   }
 
-  depends_on = [
-    kubernetes_namespace.argocd
+  values = [
+    yamlencode({
+      extraObjects = [
+        {
+          apiVersion = "argoproj.io/v1alpha1"
+          kind       = "Application"
+          metadata = {
+            name      = "assemblemonitor-eks"
+            namespace = "argocd"
+          }
+          spec = {
+            project = "default"
+            source = {
+              repoURL        = "https://github.com/Aoun62336/AssembleMonitor-DevOps.git"
+              targetRevision = "HEAD"
+              path           = "k8s/helm-chart"
+              helm = {
+                valueFiles = ["values-prod.yaml"]
+              }
+            }
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = "assemblemonitor"
+            }
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = true
+              }
+              syncOptions = [
+                "CreateNamespace=true",
+                "ServerSideApply=true"
+              ]
+            }
+            ignoreDifferences = [
+              {
+                group        = "apps"
+                kind         = "Deployment"
+                jsonPointers = ["/status/terminatingReplicas"]
+              },
+              {
+                group        = "apps"
+                kind         = "DaemonSet"
+                jsonPointers = ["/status/terminatingReplicas"]
+              },
+              {
+                group        = "apps"
+                kind         = "StatefulSet"
+                jsonPointers = ["/status/terminatingReplicas"]
+              }
+            ]
+          }
+        }
+      ]
+    })
   ]
-}
 
-# ---------------------------------------------------------
-# ArgoCD GitOps Application Setup (Using local-exec to avoid CRD plan errors)
-# ---------------------------------------------------------
-resource "null_resource" "argocd_application_apply" {
   depends_on = [
-    helm_release.argocd,
+    kubernetes_namespace.argocd,
     helm_release.external_secrets,
-    helm_release.metrics_server
+    helm_release.metrics_server,
+    kubernetes_storage_class.gp3
   ]
-
-  provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name ${aws_eks_cluster.main.name} --region ${var.aws_region} && kubectl apply -f ../k8s/argocd-application.yaml"
-  }
 }
