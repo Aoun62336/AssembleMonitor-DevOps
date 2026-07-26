19bf42a26a806b299046642c03fa8e6b35adcd6e
Aoun62336 <aounmd74@gmail.com>
2026-07-26 13:38:35 +0530

    feat: configure production Helm values, ArgoCD application deployment, and OpenTelemetry collector resources

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -24,67 +24,71 @@ resource "helm_release" "argocd" {
     value = "ClusterIP"
   }
 
-  values = [
-    yamlencode({
-      extraObjects = [
-        {
-          apiVersion = "argoproj.io/v1alpha1"
-          kind       = "Application"
-          metadata = {
-            name      = "assemblemonitor-eks"
-            namespace = "argocd"
-          }
-          spec = {
-            project = "default"
-            source = {
-              repoURL        = "https://github.com/Aoun62336/AssembleMonitor-DevOps.git"
-              targetRevision = "HEAD"
-              path           = "k8s/helm-chart"
-              helm = {
-                valueFiles = ["values-prod.yaml"]
-              }
-            }
-            destination = {
-              server    = "https://kubernetes.default.svc"
-              namespace = "assemblemonitor"
-            }
-            syncPolicy = {
-              automated = {
-                prune    = true
-                selfHeal = true
-              }
-              syncOptions = [
-                "CreateNamespace=true",
-                "ServerSideApply=true"
-              ]
-            }
-            ignoreDifferences = [
-              {
-                group        = "apps"
-                kind         = "Deployment"
-                jsonPointers = ["/status/terminatingReplicas"]
-              },
-              {
-                group        = "apps"
-                kind         = "DaemonSet"
-                jsonPointers = ["/status/terminatingReplicas"]
-              },
-              {
-                group        = "apps"
-                kind         = "StatefulSet"
-                jsonPointers = ["/status/terminatingReplicas"]
-              }
-            ]
-          }
-        }
-      ]
-    })
-  ]
-
   depends_on = [
     kubernetes_namespace.argocd,
     helm_release.external_secrets,
     helm_release.metrics_server,
     kubernetes_storage_class.gp3
+  ]
+}
+
+resource "helm_release" "argocd_apps" {
+  name       = "argocd-apps"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argocd-apps"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+
+  values = [
+    yamlencode({
+      applications = {
+        assemblemonitor-eks = {
+          namespace = "argocd"
+          project   = "default"
+          source = {
+            repoURL        = "https://github.com/Aoun62336/AssembleMonitor-DevOps.git"
+            targetRevision = "HEAD"
+            path           = "k8s/helm-chart"
+            helm = {
+              valueFiles = ["values-prod.yaml"]
+            }
+          }
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "assemblemonitor"
+          }
+          syncPolicy = {
+            automated = {
+              prune    = true
+              selfHeal = true
+            }
+            syncOptions = [
+              "CreateNamespace=true",
+              "ServerSideApply=true"
+            ]
+          }
+          ignoreDifferences = [
+            {
+              group        = "apps"
+              kind         = "Deployment"
+              jsonPointers = ["/status/terminatingReplicas"]
+            },
+            {
+              group        = "apps"
+              kind         = "DaemonSet"
+              jsonPointers = ["/status/terminatingReplicas"]
+            },
+            {
+              group        = "apps"
+              kind         = "StatefulSet"
+              jsonPointers = ["/status/terminatingReplicas"]
+            }
+          ]
+        }
+      }
+    })
+  ]
+
+  depends_on = [
+    helm_release.argocd
   ]
 }
